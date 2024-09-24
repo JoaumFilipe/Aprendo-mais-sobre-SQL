3886559cd0258383949d9803802dddd6b7483fb1
FilipeDiass <diasjoaum@gmail.com>
2024-09-24 17:10:43 -0300

    add routes get and post.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,30 @@
 import express from 'express'
+import getRoom from './db/querys/get-room.js'
+import postRoom from './db/querys/post-room.js'
 
 export const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (_req, res) => {
-  res.status(200).json({ Aprendendo: 'SQL' })
+// Rotas da aplicação
+app.get('/room?:search', async (req, res) => {
+  const search = req.query.search
+
+  try {
+    const data = await getRoom(search)
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(500).send('Error fetching data')
+  }
+})
+
+app.post('/room', async (req, res) => {
+  const dataForm = req.body
+
+  try {
+    await postRoom(dataForm)
+    res.redirect('/room')
+  } catch (err) {
+    res.status(500).send('Error fetching data')
+  }
 })
